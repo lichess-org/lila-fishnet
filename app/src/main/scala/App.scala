7368f0186a00af174861d3ad1f32f4f8cdf77458
Thanh Le <lenguyenthanh@hotmail.com>
2023-11-24 21:19:02 +0100

    Refactor out FishnetApp, prep for intergration test

--- app/src/main/scala/App.scala
+++ app/src/main/scala/App.scala
@@ -18,8 +18,14 @@ object App extends IOApp.Simple:
     for
       config <- Resource.eval(Config.load)
       _      <- Resource.eval(KamonInitiator.apply.init(config.kamon))
-      _      <- Resource.eval(Logger[IO].info(s"Starting lila-fishnet with config: $config"))
-      res    <- AppResources.instance(config.redis)
+      _      <- FishnetApp(config).run()
+    yield ()
+
+class FishnetApp(config: AppConfig)(using Logger[IO]):
+  def run(): Resource[IO, Unit] =
+    for
+      res <- AppResources.instance(config.redis)
+      _   <- Resource.eval(Logger[IO].info(s"Starting lila-fishnet with config: $config"))
       lilaClient = LilaClient(res.redisPubsub)
       monitor    = Monitor.apply
       executor <- Resource.eval(Executor.instance(lilaClient, monitor))
