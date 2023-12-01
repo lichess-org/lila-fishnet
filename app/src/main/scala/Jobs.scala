65ca8169455def5a0cac8933f1be48cfad5291f3
Thanh Le <lenguyenthanh@hotmail.com>
2023-12-01 22:21:16 +0100

    Fix cleantime instant, it == now minus 3 sconds

--- app/src/main/scala/Jobs.scala
+++ app/src/main/scala/Jobs.scala
@@ -35,4 +35,5 @@ object WorkCleaningJob:
       def run(): IO[Unit] =
         Logger[IO].info("Start cleaning job") *>
           IO.sleep(5.seconds) *>
-          (IO.realTimeInstant.flatMap(executor.clean) *> IO.sleep(3.seconds)).foreverM
+          (IO.realTimeInstant.flatMap(now => executor.clean(now.minusSeconds(3))) *>
+            IO.sleep(3.seconds)).foreverM
