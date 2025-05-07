301f03e09a9db213ef8a045e8c30bc39a915acda
Thanh Le <lenguyenthanh@hotmail.com>
2025-05-07 12:37:43 +0200

    Bump dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,10 +5,10 @@ object Dependencies {
   val lilaMaven = "lila-maven" at "https://raw.githubusercontent.com/lichess-org/lila-maven/master"
 
   object V {
-    val catsEffect = "3.6.0"
+    val catsEffect = "3.6.1"
     val chess      = "17.3.0"
-    val circe      = "0.14.12"
-    val ciris      = "3.7.0"
+    val circe      = "0.14.13"
+    val ciris      = "3.8.0"
     val fs2        = "3.12.0"
     val http4s     = "0.23.30"
     val kamon      = "2.7.7"
