dc2dfcb40d55aac5054af37cdfa45aa192d88f0b
Scala Steward <scala_steward@virtuslab.com>
2025-07-04 16:43:14 +0000

    Update scalachess, scalachess-test-kit to 17.8.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
 
   object V {
     val catsEffect = "3.6.1"
-    val chess      = "17.8.4"
+    val chess      = "17.8.5"
     val circe      = "0.14.14"
     val ciris      = "3.9.0"
     val fs2        = "3.12.0"
