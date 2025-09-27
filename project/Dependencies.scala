ca516b6a61bb6bf513dabdcd5b156534c0378ae9
Scala Steward <scala_steward@virtuslab.com>
2025-09-27 16:18:08 +0000

    Update ciris, ciris-http4s to 3.11.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val catsEffect = "3.6.3"
     val chess      = "17.9.6"
     val circe      = "0.14.14"
-    val ciris      = "3.10.0"
+    val ciris      = "3.11.0"
     val fs2        = "3.12.2"
     val http4s     = "0.23.30"
     val kamon      = "2.7.7"
