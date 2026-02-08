828b283900ca76bb09f2d7b541f150a17b392f82
Scala Steward <scala_steward@virtuslab.com>
2026-02-08 17:19:38 +0000

    Update otel4s-instrumentation-metrics to 0.15.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     val ciris        = "3.12.0"
     val fs2          = "3.12.2"
     val http4s       = "0.23.33"
-    val otel4s       = "0.14.0"
+    val otel4s       = "0.15.1"
     val otel4sHttp4s = "0.15.0"
   }
 
